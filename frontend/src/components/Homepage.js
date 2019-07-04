31ea7296063f5aeb579a5d77921dc5b136b4d8fc
hoang <vinhyenvodoi98@gmail.com>
2019-07-04 17:10:30 +0700

    add frontend

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import getWeb3 from '../Utilis/getWeb3';
 
 import '../App.css';
 
 class Homepage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      web3: {},
+      username: '',
+      password: '',
+      accounts: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async componentWillMount() {
+    this.setState({ web3: await getWeb3() });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    this.setState({
+      username: data.get('username'),
+      password: data.get('password'),
+      accounts: await this.state.web3.eth.getAccounts()
+    });
+    console.log(this.state);
+    // fetch('/api/form-submit-url', {
+    //   method: 'POST',
+    //   body: data
+    // });
   }
 
   render() {
-    return <div>hello</div>;
+    if (this.state.web3.eth !== undefined) {
+      return (
+        <div className='container'>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor='username'>Enter username</label>
+            <input id='username' name='username' type='text' />
+
+            <label htmlFor='password'>Enter your password</label>
+            <input id='password' name='password' type='password' />
+
+            <button>Send data!</button>
+          </form>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
   }
 }
 
